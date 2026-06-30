9bf104abb32637d348050558961d7813411a451a
luiscnr <loisg9@gmail.com>
2026-06-30 19:35:27 +0200

    cdom test

--- make_cdom.py
+++ make_cdom.py
@@ -112,23 +112,20 @@ class CDOMRun:
 
 
 def main(args_d):
-    print('115')
     start_date = cf.get_date_arg(args_d['start_date'])
-    print(start_date)
     end_date = cf.get_date_arg(args_d['end_date'])
-    print(end_date)
     if start_date is None:
         return
     if end_date is None:
         end_date = start_date
-    print(end_date)
+
     options = OptionsCDOM(args_d['config_file'])
-    print('126')
+
     if not options.VALID:
         return
-    print('129')
-    # cdom_options = options.get_cdom_options()
-    #
+    print('129',start_date,end_date)
+    cdom_options = options.get_cdom_options()
+    print('128')
     # work_date  = start_date
     # while work_date <= end_date:
     #     print('[INFO] --------------------------------------------------------')
@@ -149,5 +146,4 @@ if __name__ == "__main__":
     parser.add_argument('-ed', "--end_date", help="End date: YYYY-mm-dd")
     args = parser.parse_args()
     args_dict = vars(args)
-    print(args_dict)
     main(args_dict)
